4891152925f91aabc6f577cc310df4237dc23e77
Zhongyi-Lu <zhongyi_lu@outlook.com>
2022-12-23 01:56:00 -0800

    feat: add logger util

---
+++ src/main/java/com/liviz/v2/config/Utils.java
@@ -0,0 +1,14 @@
+package com.liviz.v2.config;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Utils {
+    @Bean
+    public Log getLogger() {
+        return LogFactory.getLog(getClass());
+    }
+}
